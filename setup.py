1d65835df8d4f72c7f329bd6ab46ac4f3b16d449
Covao / Koichi Kobayashi <kkoichimx4@hotmail.com>
2025-08-01 04:52:08 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chibiui',
+    version='0.8.0',
+    author='covao, Koichi Kobayashi',
+    license='MIT',
+    author_email='covao.dev@gmail.com',
+    url='https://github.com/covao/ChibiUI',
+    description='Tiny Python GUI framework for easy user interfaces with Tkinter.',
+    python_requires='>=3.7',
+    install_requires=[]
+)
